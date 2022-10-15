580be6076d2fee3ed0afc64699840539ac415c7a
obidike ifeanyi <ifeanyiobidike1@gmail.com>
2022-10-14 23:46:43 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+	char c = 'a';
+
+
+
+	while (c <= 'z')
+
+	{
+
+		putchar(c);
+
+		c++;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
