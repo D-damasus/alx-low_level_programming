11dabd87204a9b8fe25ebde8f69009df4a82bad3
obidike ifeanyi <ifeanyiobidike1@gmail.com>
2022-10-16 13:32:45 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *  main - the containing func
+ *  Return: 0 if succesful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
