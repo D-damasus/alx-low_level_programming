456557fdff19e89fdd34abfcde1fe6c17e11e2e3
obidike ifeanyi <ifeanyiobidike1@gmail.com>
2022-10-16 13:27:15 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Function starts
+ *
+ *  Return: Function closes
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
