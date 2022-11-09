74827857dd86865c7397113c275120b2e94e3ebd
ifeanyi obidike <ifeanyiobidike1@gmail.com>
2022-11-08 16:22:26 -0800

    Task o

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -2,10 +2,10 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
+ *  @c: The character to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ *  Return: On success 1.
+ *   On error, -1 is returned, and errno is set appropriately
  */
 int _putchar(char c)
 {
